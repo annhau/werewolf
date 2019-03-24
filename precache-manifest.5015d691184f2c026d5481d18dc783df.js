8135639222cf4d509df37d82848e03a60333274c
Your Name <you@example.com>
2019-03-24 18:19:07 +0700

    Updates

---
+++ precache-manifest.5015d691184f2c026d5481d18dc783df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7667dbbf3a7675b09fc3",
+    "url": "/werewolf/static/js/runtime~main.1b490474.js"
+  },
+  {
+    "revision": "696d6326f6efa6c466af",
+    "url": "/werewolf/static/js/main.5fc04441.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/werewolf/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "696d6326f6efa6c466af",
+    "url": "/werewolf/static/css/main.7eaeb15d.chunk.css"
+  },
+  {
+    "revision": "0503b9b8ef46f1cf75ac4bb27128c907",
+    "url": "/werewolf/index.html"
+  }
+];
